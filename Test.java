3bb98ca0aaae347e8e71abebf91250b4bc2bfaf4
anotre <notrevispe@yandex.ru>
2022-07-25 20:32:43 +0500

    [l6hw] Changed the method of storing test data and processing its results

--- Test.java
+++ Test.java
@@ -2,52 +2,64 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        String[] questions = {
-            "Что такое массив?",
-            "Какое из перечисленных ключевых слов не существует в языке Java?",
-            "Какое утверждение характеризует enum (перечисление)?"
-        }; 
-        String[][] answerOptions = {
+        final int ANSWER_OPTION = 1;
+        final int RIGHT_ANSWER = 2;
+        final int ANSWER_INDEX = 0;
+
+        String[][][] test = {
             {
-                "Набор элементов одного из ссылочных типов, на который ссылаются по одному имени.",
-                "Набор элементов одного из примитивных типов, на который ссылаются по одному имени.",
-                "Набор элементов любого типа, на который ссылаются по одному имени.",
-                "Набор элементов одного типа, на который ссылаются по одному имени."
+                {"Что такое массив?"},
+                {
+                    "Набор элементов одного из ссылочных типов, на который ссылаются по одному имени.",
+                    "Набор элементов одного из примитивных типов, на который ссылаются по одному имени.",
+                    "Набор элементов любого типа, на который ссылаются по одному имени.",
+                    "Набор элементов одного типа, на который ссылаются по одному имени."
+                },
+                {"4"}
             },
             {
-                "for",
-                "elseif",
-                "class",
-                "byte"
+                {"Какое из перечисленных ключевых слов не существует в языке Java?"},
+                {
+                    "for",
+                    "elseif",
+                    "class",
+                    "byte"
+                },
+                {"2"}
             },
             {
-                "Перечисления предоставляют возможные значения для какого-либо элемента",
-                "Перечисления представляют собой дополняемые объекты",
-                "Перечисления объявляются при помощи ключевого слова new",
-                "Перечисления объявляются как обычные переменные - внутри методов"
+                {"Какое утверждение характеризует enum (перечисление)?"},
+                {
+                    "Перечисления предоставляют возможные значения для какого-либо элемента",
+                    "Перечисления представляют собой дополняемые объекты",
+                    "Перечисления объявляются при помощи ключевого слова new",
+                    "Перечисления объявляются как обычные переменные - внутри методов"
+                },
+                {"1"}
             }
         };
-        int[] keys = {4, 2, 1};
 
-        for (int i = 0; i < questions.length; i++) {
-            System.out.println((i + 1) + ". " + questions[i]);
-            for (int j = 0; j < answerOptions[i].length; j++) {
-                System.out.println("   " + (j + 1) + ") " + answerOptions[i][j]);
+        for (int qnNumber = 0; qnNumber < test.length; qnNumber++) {
+            for (int qnComponent = 0; qnComponent < test[qnNumber].length - 1; qnComponent++) {
+                final String INDENT = (qnComponent == ANSWER_OPTION) ? "   " : "";
+
+                for (int item = 0; item < test[qnNumber][qnComponent].length; item++) {
+                    System.out.println(INDENT + test[qnNumber][qnComponent][item]);
+                }
             }
         }
 
-        int[] answers = new int[questions.length];
+        int[] answers = new int[test.length];
         Scanner inputScanner = new Scanner(System.in);
 
-        for (int i = 0; i < questions.length; i++) {
+        System.out.println("Пожалуйста, введите ответы на вопросы:");
+        for (int i = 0; i < test.length; i++) {
             answers[i] = inputScanner.nextInt();
-            System.out.println(answers[i]);
         }
         inputScanner.close();
-
-        System.out.println("Пожалуйста, введите ответы на вопросы:");
-        for (int i = 0; i < questions.length; i++) {
-            String result = (answers[i] == keys[i]) ? "верно" : "неверно";
+     
+        for (int i = 0; i < test.length; i++) {
+            String result = (Integer.parseInt(test[i][RIGHT_ANSWER][ANSWER_INDEX]) == answers[i]) ? "верно" : "неверно";
             System.out.println("Вопрос №" + (i + 1) + " - " + result + ";");
         }
     }
